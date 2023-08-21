7877acec693b18a510aa233a02ae2febf1474c78
Serhii Yunnikov <yunnikov@icloud.com>
2023-08-21 13:48:56 +0300

    gulp plumber, notify for errors handling and notification

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,9 +1,9 @@
 import replace from "gulp-replace";
-// import plumber from "gulp-plumber";
-// import notify from "gulp-notify";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
 
 export const plugins = {
   replace: replace,
-  // plumber: plumber,
-  // notify: notify
+  plumber: plumber,
+  notify: notify
 }
