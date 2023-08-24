59740d46b64184b5de1a86a9d15c2ae5070dfe0d
Serhii Yunnikov <yunnikov@icloud.com>
2023-08-24 18:50:37 +0300

    webp optimization compressing the images

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -10,10 +10,13 @@ export const path = {
     js: `${buildFolder}/js/`,
     css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
+    images: `${buildFolder}/img/`,
     files: `${buildFolder}/files/`
   },
   src: {
     js: `${srcFolder}/js/app.js`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
+    svg: `${srcFolder}/img/**/*.svg`,
     scss: `${srcFolder}/scss/style.scss`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
@@ -22,6 +25,7 @@ export const path = {
     js: `${srcFolder}/js/**/*.js`,
     scss: `${srcFolder}/scss/**/*.scss`,
     html: `${srcFolder}/**/*.html`,
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,ico,webp}`,
     files: `${srcFolder}/files/**/*.*`,
   },
   clean: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,14 @@ global.app = {
   plugins: plugins
 }
 
-//import tasks
+//import of tasks
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
+import { images } from "./gulp/tasks/images.js";
 
 //files changes observer
 function watcher() {
@@ -26,9 +27,10 @@ function watcher() {
   gulp.watch(path.watch.html, html) ;
   gulp.watch(path.watch.scss, scss);
   gulp.watch(path.watch.js, js);
+  gulp.watch(path.watch.images, images);
 }
 
-const mainTasks = gulp.parallel(copy, html, scss, js);
+const mainTasks = gulp.parallel(copy, html, scss, js, images);
 
 //development of task execution scripts
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
